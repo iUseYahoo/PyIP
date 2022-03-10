1082f41c8062691f342e90beb0a40416b281812a
Nicholas <64442818+iUseYahoo@users.noreply.github.com>
2022-03-10 14:59:18 +1100

    Update pyip.py

--- pyip.py
+++ pyip.py
@@ -2,12 +2,31 @@ from columnar import columnar
 import requests
 import os
 from art import *
+import sys
 
-os.system('cls')
+def clear():
+  if os.name == "nt":
+    os.system("cls")
+  elif os.name == "posix":
+    os.system("clear")
+  else:
+    print(f"OS: {os.name} | Could not clear the console but will continue.\n")
+    pass
+
+
+clear()
 print(text2art("PyIP") + "Made by github.com/iUseYahoo\nIf you want to track yourself just press enter.\n")
 
 class mainClass:
     def trackIP(grabip):
+
+
+        if grabip == "":
+          print("[+] Tracking your own IP.")
+        else:
+          print(f"[*] Tracking IP: {grabip}\n")
+          pass
+          
         showcont = requests.get(f"http://ip-api.com/json/{grabip}?fields=continent")
         showcoun = requests.get(f"http://ip-api.com/json/{grabip}?fields=country")
         showregi = requests.get(f"http://ip-api.com/json/{grabip}?fields=regionName")
@@ -24,8 +43,7 @@ class mainClass:
             ["Post Code (ZIP) ", showpost.text.strip('}{":')],
             ["Internet Provider (ISP) ", showisp.text.strip('}{":')]
         ]
-
-        print(f"[*] Tracking IP: {grabip}\n")
+      
         print(columnar(table_data, table_headers))
 
     def main():
@@ -33,11 +51,12 @@ class mainClass:
             grabip = input("Enter IP: ")
 
             if grabip == "clear":
-                os.system('cls')
+                clear()
                 print(text2art("PyIP") + "Made by github.com/iUseYahoo\nIf you want to track yourself just press enter.\n")
-
+            elif grabip == "exit":
+              sys.exit()
             else:
-                mainClass.trackIP(grabip)
+              mainClass.trackIP(grabip)
             
 if __name__ == '__main__':
     mainClass.main()
